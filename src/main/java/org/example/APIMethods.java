b67ca5803f53655e8772a447e83c47841c742a83
Arghajit Singha <arghajit.singha2@ibm.com>
2024-03-20 18:10:28 +0530

    commiting the full Framework - Commit 03

--- src/main/java/org/example/APIMethods.java
+++ src/main/java/org/example/APIMethods.java
@@ -1,2 +1,122 @@
-package org.example;public class APIMethods {
+package org.example;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class APIMethods {
+    CommonMethods main = new CommonMethods();
+    public void getAPICall(String propName, Integer expectedStatusCode, String outputFileName) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        String listUsersPath = main.readPropertiesFile(propName);
+        Response response = given()
+                .baseUri(baseURL)
+                .when()
+                .get(listUsersPath)
+                .then()
+                .statusCode(expectedStatusCode)
+                .extract().response();
+        main.writeFile(response, "responses/" + outputFileName);
+    }
+
+    public void getAPICall(String propName, String outputFileName) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        String listUsersPath = main.readPropertiesFile(propName);
+        Response response = given()
+                .baseUri(baseURL)
+                .when()
+                .get(listUsersPath)
+                .then()
+                .extract().response();
+        main.writeFile(response, "responses/" + outputFileName);
+    }
+
+    public int postAPICall(String propName, String outputFileName, String reqBody, String var) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        String path = main.readPropertiesFile(propName);
+        File requestBodyFile = new File("src/test/resources/jsonFolder/" + reqBody);
+        Response response = given()
+                .baseUri(baseURL)
+                .body(requestBodyFile)
+                .when()
+                .post(path)
+                .then()
+                .extract().response();
+        int variable = response.jsonPath().getInt(var);
+        main.writeFile(response, "responses/" + outputFileName);
+        return variable;
+    }
+
+    public void postAPICall(String propName, String outputFileName, String reqBody) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        String path = main.readPropertiesFile(propName);
+        File requestBodyFile = new File("src/test/resources/jsonFolder/" + reqBody);
+        Response response = given()
+                .baseUri(baseURL)
+                .body(requestBodyFile)
+                .when()
+                .post(path)
+                .then()
+                .extract().response();
+        main.writeFile(response, "responses/" + outputFileName);
+    }
+
+    public void putAPICall(String reqBody, String outputFileName, String path) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        File reqBodyFile = new File("src/test/resources/jsonFolder/" + reqBody);
+        Response updatePutResponse = given()
+                .baseUri(baseURL)
+                .body(reqBodyFile)
+                .when()
+                .put(path)
+                .then()
+                .extract().response();
+        main.writeFile(updatePutResponse, "responses/" + outputFileName);
+    }
+
+    public void patchAPICall(String reqBody, String outputFileName, String path) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        File reqBodyFile = new File("src/test/resources/jsonFolder/" + reqBody);
+        Response updatePutResponse = given()
+                .baseUri(baseURL)
+                .body(reqBodyFile)
+                .when()
+                .patch(path)
+                .then()
+                .extract().response();
+        main.writeFile(updatePutResponse, "responses/" + outputFileName);
+    }
+
+    public void deleteAPICall(String path, Integer expectedStatusCode) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        Response deleteResponse = given()
+                .baseUri(baseURL)
+                .when()
+                .delete(path)
+                .then()
+                .statusCode(expectedStatusCode)
+                .extract().response();
+        System.out.println("API Response Status Code is : " + deleteResponse.statusCode());
+        System.out.println(deleteResponse.getBody().asString());
+    }
+
+    public void postAPICallWithReqFile(String propName, String reqBody,Integer expectedStatusCode, String outputFileName) {
+        String baseURL = main.readPropertiesFile("baseUrl");
+        String registerPath = main.readPropertiesFile(propName);
+        File reqBodyFile = new File("src/test/resources/jsonFolder/" + reqBody);
+        Response response = given()
+                .baseUri(baseURL)
+                .contentType(ContentType.JSON)
+                .body(reqBodyFile)
+                .when()
+                .post(registerPath)
+                .then()
+                .statusCode(expectedStatusCode)
+                .extract().response();
+        main.writeFile(response, "responses/" + outputFileName);
+    }
+
 }
--- src/test/java/APITesting.java
+++ src/test/java/APITesting.java
@@ -1,163 +1,65 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.example.CommonMethods;
-import java.io.File;
+import org.example.*;
+import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 
 
 public class APITesting {
     static CommonMethods main = new CommonMethods();
+    static APIMethods api = new APIMethods();
 
+    @Test
     public static void listUsers() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String listUsersPath = main.readPropertiesFile("listUsers");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(listUsersPath)
-                .then()
-                .statusCode(200)
-                .extract().response();
-        main.writeFile(response, "responses/ListUsers.json");
-
+        api.getAPICall("listUsers", 200, "ListUsersResponse.json");
     }
 
+    @Test
     public static void singleUsers() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String singleUserPath = main.readPropertiesFile("singleUser");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(singleUserPath)
-                .then()
-                .statusCode(200)
-                .extract().response();
-        main.writeFile(response, "responses/SingleUser.json");
-
+        api.getAPICall("singleUser", 200, "SingleUserResponse.json");
     }
 
+    @Test
     public static void userNotFound() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String userNotFoundPath = main.readPropertiesFile("userNotFound");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(userNotFoundPath)
-                .then()
-                .statusCode(404)
-                .extract().response();
-        main.writeFile(response, "responses/UserNotFound.json");
+        api.getAPICall("userNotFound", 404, "UserNotFoundResponse.json");
 
     }
 
+    @Test
     public static void listResource() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String listResourcePath = main.readPropertiesFile("listResource");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(listResourcePath)
-                .then()
-                .extract().response();
-        main.writeFile(response, "responses/ListResource.json");
-
+        api.getAPICall("listResource", "ListResourceResponse.json");
     }
 
+    @Test
     public static void singleResource() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String singleResourcePath = main.readPropertiesFile("singleResource");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(singleResourcePath)
-                .then()
-                .extract().response();
-        main.writeFile(response, "responses/SingleResource.json");
+        api.getAPICall("singleResource", "SingleResourceResponse.json");
 
     }
 
+    @Test
     public static void singleResourceNotFound() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String singleResourceNotFoundPath = main.readPropertiesFile("singleResourceNotFound");
-        Response response = given()
-                .baseUri(baseURL)
-                .when()
-                .get(singleResourceNotFoundPath)
-                .then()
-                .extract().response();
-        main.writeFile(response, "responses/SingleResourceNotFound.json");
-
+        api.getAPICall("singleResourceNotFound", "SingleResourceNotFoundResponse.json");
     }
 
+    @Test
     public static void userAction() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String createUserPath = main.readPropertiesFile("createUser");
-        File requestBodyFile = new File("src/test/resources/jsonFolder/createUser.json");
-        Response createResponse = given()
-                .baseUri(baseURL)
-                .body(requestBodyFile)
-                .when()
-                .post(createUserPath)
-                .then()
-                .extract().response();
-        int id = createResponse.jsonPath().getInt("id");
-        main.writeFile(createResponse, "responses/CreateUserResponse.json");
+        int id = api.postAPICall("createUser", "CreateUserResponse.json", "createUser.json", "id");
 
+        api.putAPICall("updateUser.json", "UpdateUserByPutResponse.json", main.readPropertiesFile("createUser") + "/" + id);
 
+        api.patchAPICall("updateUser.json", "UpdateUserByPutResponse.json", main.readPropertiesFile("createUser") + "/" + id);
 
-        File updatePutBodyFile = new File("src/test/resources/jsonFolder/updateUser.json");
-        Response updatePutResponse = given()
-                .baseUri(baseURL)
-                .body(updatePutBodyFile)
-                .when()
-                .put(createUserPath + "/" + id)
-                .then()
-                .extract().response();
-        main.writeFile(updatePutResponse, "responses/UpdateUserByPutResponse.json");
-
-
-
-        File updatePatchBodyFile = new File("src/test/resources/jsonFolder/updateUser.json");
-        Response updatePatchResponse = given()
-                .baseUri(baseURL)
-                .body(updatePatchBodyFile)
-                .when()
-                .put(createUserPath + "/" + id)
-                .then()
-                .extract().response();
-        main.writeFile(updatePatchResponse, "responses/UpdateUserByPatchResponse.json");
-
-
-
-        Response deleteResponse = given()
-                .baseUri(baseURL)
-                .when()
-                .delete(createUserPath + "/" + id)
-                .then()
-                .statusCode(204)
-                .extract().response();
-        System.out.println("API Response Status Code is : " + deleteResponse.statusCode());
-        System.out.println(deleteResponse.getBody().asString());
+        api.deleteAPICall(main.readPropertiesFile("createUser") + "/" + id, 204);
 
     }
 
+    @Test
     public static void successfulRegister() throws InterruptedException {
-        String baseURL = main.readPropertiesFile("baseUrl");
-        String registerPath = main.readPropertiesFile("register");
-        File successfulRegisterBodyFile = new File("src/test/resources/jsonFolder/successfulRegister.json");
-        Response response = given()
-                .baseUri(baseURL)
-                .contentType(ContentType.JSON)
-                .body(successfulRegisterBodyFile)
-                .when()
-                .post(registerPath)
-                .then()
-                .statusCode(200)
-                .extract().response();
-        main.writeFile(response, "responses/SuccessfulRegisterResponse.json");
+        api.postAPICallWithReqFile("register", "successfulRegister.json", 200, "SuccessfulRegisterResponse.json");
 
     }
 
+    @Test
     public static void unsuccessfulRegister() throws InterruptedException {
         String baseURL = main.readPropertiesFile("baseUrl");
         String registerPath = main.readPropertiesFile("register");
@@ -173,8 +75,10 @@ public class APITesting {
                 .statusCode(400)
                 .extract().response();
         main.writeFile(response, "responses/UnsuccessfulRegisterResponse.json");
+
     }
 
+    @Test
     public static void unsuccessfulLogin() throws InterruptedException {
         String baseURL = main.readPropertiesFile("baseUrl");
         String loginPath = main.readPropertiesFile("login");
@@ -193,6 +97,7 @@ public class APITesting {
 
     }
 
+    @Test
     public static void delayedResponse() throws Exception {
         String baseURL = main.readPropertiesFile("baseUrl");
         String delayedResponsePath = main.readPropertiesFile("delayedResponse");
@@ -208,17 +113,4 @@ public class APITesting {
         main.writeFile(response, "responses/DelayedResponse.json");
     }
 
-    public static void main(String[] args) throws Exception {
-        listUsers();
-        singleUsers();
-        userNotFound();
-        listResource();
-        singleResource();
-        singleResourceNotFound();
-        userAction();
-        successfulRegister();
-        unsuccessfulRegister();
-        unsuccessfulLogin();
-        delayedResponse();
-    }
 }
